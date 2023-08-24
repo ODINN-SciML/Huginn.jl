7f0ab8a58286e3699bad5714fe2f1c21b4eb08fa
Facundo Sapienza <fsapienza@berkeley.edu>
2023-08-24 10:58:01 -0700

    [testing] Adding Python dependencies to shortcut conda env

--- src/Huginn.jl
+++ src/Huginn.jl
@@ -30,6 +30,30 @@ const global root_dir::String = dirname(Base.current_project())
 const global root_plots::String = joinpath(root_dir, "plots")
 
 # ##############################################
+# ############  PYTHON LIBRARIES  ##############
+# ##############################################
+
+# This should be imported directly from Sleipnir.jl, but currently this is not working
+# Re-importing these variables here is a partial solution
+
+const netCDF4::PyObject = PyNULL()
+const cfg::PyObject = PyNULL()
+const utils::PyObject = PyNULL()
+const workflow::PyObject = PyNULL()
+const tasks::PyObject = PyNULL()
+const global_tasks::PyObject = PyNULL()
+const graphics::PyObject = PyNULL()
+const bedtopo::PyObject = PyNULL()
+const millan22::PyObject = PyNULL()
+const MBsandbox::PyObject = PyNULL()
+const salem::PyObject = PyNULL()
+
+# Essential Python libraries
+const xr::PyObject = PyNULL()
+const rioxarray::PyObject = PyNULL()
+const pd::PyObject = PyNULL()
+
+# ##############################################
 # ############  HUGINN LIBRARIES  ##############
 # ##############################################
 
