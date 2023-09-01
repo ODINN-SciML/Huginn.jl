d26a8cba6fec62d5c9eed7b431c5be2124158869
Facundo Sapienza <fsapienza@berkeley.edu>
2023-09-01 00:08:39 +0000

    Add Python config skeleton for stand-alone installation

--- src/Huginn.jl
+++ src/Huginn.jl
@@ -30,8 +30,32 @@ const global root_dir::String = dirname(Base.current_project())
 const global root_plots::String = joinpath(root_dir, "plots")
 
 # ##############################################
+# ############  PYTHON LIBRARIES  ##############
+# ##############################################
+
+const netCDF4::PyObject = PyNULL()
+const cfg::PyObject = PyNULL()
+const utils::PyObject = PyNULL()
+const workflow::PyObject = PyNULL()
+const tasks::PyObject = PyNULL()
+const global_tasks::PyObject = PyNULL()
+const graphics::PyObject = PyNULL()
+const bedtopo::PyObject = PyNULL()
+const millan22::PyObject = PyNULL()
+const MBsandbox::PyObject = PyNULL()
+const salem::PyObject = PyNULL()
+
+# Essential Python libraries
+const xr::PyObject = PyNULL()
+const rioxarray::PyObject = PyNULL()
+const pd::PyObject = PyNULL()
+
+# ##############################################
 # ############  HUGINN LIBRARIES  ##############
 # ##############################################
+
+# Include setup for Python in case this does not already exist
+include("setup/config.jl")
 
 include("parameters/SolverParameters.jl")
 # All structures and functions related to Ice flow models
---
+++ src/setup/config.jl
@@ -0,0 +1,40 @@
+function __init__()
+
+    # Create structural folders if needed
+    OGGM_path = joinpath(homedir(), "Python/OGGM_data")
+    if !isdir(OGGM_path)
+        mkpath(OGGM_path)
+    end
+
+    println("Initializing Python libraries...")
+
+    # Load Python packages
+    try
+        copy!(netCDF4, pyimport("netCDF4"))
+        copy!(cfg, pyimport("oggm.cfg"))
+        copy!(utils, pyimport("oggm.utils"))
+        copy!(workflow, pyimport("oggm.workflow"))
+        copy!(tasks, pyimport("oggm.tasks"))
+        copy!(global_tasks, pyimport("oggm.global_tasks"))
+        copy!(graphics, pyimport("oggm.graphics"))
+        copy!(bedtopo, pyimport("oggm.shop.bedtopo"))
+        copy!(millan22, pyimport("oggm.shop.millan22"))
+        copy!(MBsandbox, pyimport("MBsandbox.mbmod_daily_oneflowline"))
+        copy!(salem, pyimport("salem"))
+        copy!(pd, pyimport("pandas"))
+        copy!(xr, pyimport("xarray"))
+        copy!(rioxarray, pyimport("rioxarray"))
+    catch e
+        @warn "It looks like you have not installed and/or activated the virtual Python environment. \n 
+        Please follow the guidelines in: https://github.com/ODINN-SciML/ODINN.jl#readme"
+        @warn exception=(e, catch_backtrace())
+    end
+
+end
+
+function clean()
+    atexit() do
+        run(`$(Base.julia_cmd())`)
+    end
+    exit()
+ end
