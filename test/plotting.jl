0feb072946c5688cbdc84d41c4af74951d4db180
mleseach <113145496+mleseach@users.noreply.github.com>
2025-04-29 16:42:24 +0200

    improve plotting test

--- test/plotting.jl
+++ test/plotting.jl
@@ -26,14 +26,6 @@ function plot_analysis_flow_parameters_test()
     A_values = [8.5e-20]
     n_values = [3.0]
 
-
-    try
-        plot_analysis_flow_parameters(prediction, A_values, n_values)
-        @test true  # Test passes if no error is thrown
-    catch e
-        println("Error occurred: ", e)
-        @test false  # Test fails if any error is caught
-
-    end
-
+    plot_analysis_flow_parameters(prediction, A_values, n_values)
+    @test true  # Test passes if no error is thrown
 end
