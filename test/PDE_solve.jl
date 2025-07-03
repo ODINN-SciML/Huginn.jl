32e88fae3795722f4d27597af3db43fbd45c270f
Alban Gossard <alban.paul.gossard@gmail.com>
2025-07-03 15:48:06 +0200

    Remove duplicate runs in CI (#91)
    * remove duplicate runs in CI
    
    * update tests to get notified when specific portions become type stable

--- test/PDE_solve.jl
+++ test/PDE_solve.jl
@@ -76,7 +76,7 @@ function pde_solve_test(; rtol::F, atol::F, save_refs::Bool=false, MB::Bool=fals
 
     # We retrieve some glaciers for the simulation
     glaciers = initialize_glaciers(rgi_ids, params)
-    # JET.@test_opt target_modules=(Sleipnir,Muninn,Huginn) initialize_glaciers(rgi_ids, params) # For the moment this is not type stable because of the readings (type of CSV files and RasterStack cannot be determined at compilation time)
+    # JET.@test_opt broken=true target_modules=(Sleipnir,Muninn,Huginn) initialize_glaciers(rgi_ids, params) # For the moment this is not type stable because of the readings (type of CSV files and RasterStack cannot be determined at compilation time)
 
     # We create an ODINN prediction
     prediction = Prediction(model, glaciers, params)
@@ -87,7 +87,7 @@ function pde_solve_test(; rtol::F, atol::F, save_refs::Bool=false, MB::Bool=fals
     @time run!(prediction)
 
     # Test below is not ready yet
-    # JET.@test_opt target_modules=(Sleipnir,Muninn,Huginn) Huginn.batch_iceflow_PDE!(1, prediction) # Call only the core of run! because saving to JLD2 file is not type stable and GC interferes with JET
+    JET.@test_opt broken=true target_modules=(Sleipnir,Muninn,Huginn) Huginn.batch_iceflow_PDE!(1, prediction) # Call only the core of run! because saving to JLD2 file is not type stable and GC interferes with JET
 
     # /!\ Saves current run as reference values
     if save_refs
@@ -164,7 +164,7 @@ function TI_run_test!(save_refs::Bool = false; rtol::F, atol::F) where {F <: Abs
     glacier_idx = 1
 
     glaciers = initialize_glaciers(rgi_ids, params)
-    # JET.@test_opt target_modules=(Sleipnir,Muninn,Huginn) initialize_glaciers(rgi_ids, params)[1] # For the moment this is not type stable because of the readings (type of CSV files and RasterStack cannot be determined at compilation time)
+    JET.@test_opt target_modules=(Sleipnir,Muninn,Huginn) initialize_glaciers(rgi_ids, params)[1]
 
     glacier = glaciers[glacier_idx]
 
@@ -172,12 +172,12 @@ function TI_run_test!(save_refs::Bool = false; rtol::F, atol::F) where {F <: Abs
     simulation = (;model, glaciers)
 
     cache = init_cache(model, simulation, glacier_idx, params)
-    # JET.@test_opt init_cache(model, simulation, glacier_idx, params) # For the moment this is not type stable because of the readings (type of CSV files and RasterStack cannot be determined at compilation time)
+    JET.@test_opt init_cache(model, simulation, glacier_idx, params)
 
     t = 2015.0
 
     MB_timestep!(cache, model, glacier, params.solver.step, t)
-    # JET.@test_opt target_modules=(Sleipnir,Muninn,Huginn) MB_timestep!(cache, model, glacier, params.solver.step, t) # RasterStack manipulation is type unstable, so for the moment this test is deactivated
+    # JET.@test_opt broken=true target_modules=(Sleipnir,Muninn,Huginn) MB_timestep!(cache, model, glacier, params.solver.step, t) # RasterStack manipulation is type unstable, so for the moment this test is deactivated
 
     apply_MB_mask!(cache.iceflow.H, cache.iceflow)
     JET.@test_opt target_modules=(Sleipnir,Muninn,Huginn) apply_MB_mask!(cache.iceflow.H, cache.iceflow)
